75598301d286f216f86df5f16152a891540d2904
giallon <nagiallombardo@gmail.com>
2018-12-18 11:26:57 +0100

    change launcher

--- app/src/main/java/fr/wildcodeschool/serviceobb/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/serviceobb/MainActivity.java
@@ -2,12 +2,37 @@ package fr.wildcodeschool.serviceobb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG  = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        URI fileName = this.getObbDir().toURI();
+        Log.d(TAG, fileName.toString());
+        File obb = new File(fileName);
+        boolean open_failed = false;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(obb));
+            open_failed = false;
+            Log.d(TAG, "open success");
+        } catch (IOException e) {
+            open_failed = true;
+        }
+
+        if (open_failed) {
+            // request R36EAD_EXTERNAL_STORAGE permission before reading OBB file
+           Log.d(TAG, "open failed");
+        }
     }
 }
